8d3d20c85eb883793e938b20c676caa9891251c0
aounmustafa <85194128+aounmustafa@users.noreply.github.com>
2021-07-05 15:16:18 +0500

    Update signup.blade.php

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -9,8 +9,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="{{asset('/css/signup.css')}}">
-    <script src="{{asset('/js/signup.js')}}"></script> 
+    <link rel="stylesheet" href="/css/signup.css">
+    <script src="/js/signup.js"></script> 
 </head>
 <body>
         <div class="container">
