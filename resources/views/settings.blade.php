fda96254c3f9a2db2df39c9bb677da125023fdfb
aounmustafa <85194128+aounmustafa@users.noreply.github.com>
2021-07-05 15:18:27 +0500

    Update settings.blade.php

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -9,8 +9,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="{{asset('/css/signup.css')}}">
-    <script src="{{asset('/js/settings.js')}}"></script> 
+    <link rel="stylesheet" href="/css/signup.css">
+    <script src="js/settings.js"></script> 
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light mb-3" id="nav">
