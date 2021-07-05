ce4de766af027bbcece09a5f0fb6878b9be207b4
aounmustafa <85194128+aounmustafa@users.noreply.github.com>
2021-07-05 14:44:26 +0500

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,8 +9,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-<link rel="stylesheet" href="{{asset('/css/login.css')}}"> 
-<script src="{{asset('/js/login.js')}}"></script> 
+<link rel="stylesheet" href="/css/login.css"> 
+<script src="asset('/js/login.js')}}"></script> 
 </head>
 
 <body>
