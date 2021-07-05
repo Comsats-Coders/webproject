5a8b4f42e892bb84ecece7f950cd81cfc62f0d82
aounmustafa <85194128+aounmustafa@users.noreply.github.com>
2021-07-05 14:46:23 +0500

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,7 +10,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 <link rel="stylesheet" href="/css/login.css"> 
-<script src="asset('/js/login.js')}}"></script> 
+<script src="/js/login.js"></script> 
 </head>
 
 <body>
